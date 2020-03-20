cb63ad37d2071a93dfa2fc26dcb8e2e4b8244a9d
Ильдар Сиразутдинов <i.sirazutdinov@pflb.ru>
2020-03-20 15:43:42 +0300

    initial commit - test 1 finished

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,20 +1,54 @@
 package tests;
 
+import io.qameta.allure.Step;
 import models.Planet;
 import org.junit.BeforeClass;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class BaseTest {
     RestTemplate restTemplate;
+    private static Logger logger = Logger.getLogger(String.valueOf(BaseTest.class));
 
 
     @BeforeClass
     public static void setUp() {
     }
 
+    @Step("Получем планету {planetNumber}")
     public Planet getPlanet(String planetNumber) {
-        restTemplate = new RestTemplate();
-        Planet planet = restTemplate.getForObject("https://swapi.co/api/planets/" + planetNumber, Planet.class);
+        Planet planet = null;
+        try {
+            restTemplate = new RestTemplate();
+            planet = restTemplate.getForObject("https://swapi.co/api/planets/" + planetNumber, Planet.class);
+        } catch (Exception e) {
+            logger.warning("Не удалось получить планету");
+        }
         return planet;
+    }
+
+    @Step("Проверяем, происходят ли какие действия на планете {planet} в {episode} серии фильма")
+    public void printIsThereSomeActionOnAPlanetXInEpisodeY(Planet planet, Integer episode) {
+        if (isThereSomeActionOnAPlanetXInEpisodeY(planet, episode)) {
+            logger.info("Действия на планете "
+                    + planet.getName() + " происходят в эпизоде " + episode);
+        } else {
+            logger.info("Действия на планете "
+                    + planet.getName() + " НЕ происходят в эпизоде " + episode);
+        }
+    }
+
+
+    public Boolean isThereSomeActionOnAPlanetXInEpisodeY(Planet planet, Integer episode) {
+        String[] filmsWithPlanet = planet.getFilms();
+        logger.info("Список фильмов с планетой " + planet.getName() + " " + Arrays.toString(planet.getFilms()));
+        int i = 0;
+        while (i < filmsWithPlanet.length) {
+            if (filmsWithPlanet[i].contains("/films/" + episode + "/")) return true;
+            i++;
+        }
+        return false;
     }
 }
--- src/test/java/tests/tcs/TC1Tatooine.java
+++ src/test/java/tests/tcs/TC1Tatooine.java
@@ -1,21 +1,27 @@
 package tests.tcs;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Story;
 import models.Planet;
 import org.junit.Test;
 import tests.BaseTest;
 
+import java.util.logging.Logger;
+
 public class TC1Tatooine extends BaseTest {
+    private static Logger logger = Logger.getLogger(String.valueOf(TC1Tatooine.class));
+    private int episode;
 
 
-    /**
-     * Выбираем планету «Tatooine» и проверяем, происходят ли какие действия на планете «Tatooine» в 4 серии фильма.
-     */
     @Test
+    @Description("Выбираем планету «Tatooine» и проверяем, происходят ли какие действия на планете «Tatooine» в 4 серии фильма")
+    @Story("TC1: Tatooine")
     public void isThereSomeActionOnTatooineInEpisode4Test() {
-        //Выбираем планету «Tatooine» и выводим в логи
+        logger.info("Выбираем планету «Tatooine» и выводим");
         Planet planet = getPlanet("1");
-        System.out.println(planet);
+        logger.info(String.valueOf(planet));
 
-
+        episode = 4;
+        printIsThereSomeActionOnAPlanetXInEpisodeY(planet, episode);
     }
 }
